938e7d8b8cd7df295f19af53552069840174f67d
Krooby_ <jkocman07@gmail.com>
2023-12-31 15:21:02 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,19 @@
     <link rel="stylesheet" href="https://api.ssps.cajthaml.eu/file/64d9f18f79dad891e8a07c73">
   </head>
   <body>
+    <Header />
     <router-view />
   </body>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
+export default{
+  components: {
+    Header,
+  }
+};
 </script>
 
 <style>
